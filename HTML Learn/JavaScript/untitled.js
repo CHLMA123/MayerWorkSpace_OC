4f019b5865ecb94e922a9f63b417f5e9c17ee5e2
CHLMA123 <chlma2015@foxmail.com>
2017-02-15 15:24:42 +0800

    HTML Learn

---
+++ HTML Learn/JavaScript/untitled.js
@@ -0,0 +1,3 @@
+document.write("...javascript 脚本语言...");
+document.write("<hr/>");
+document.write("JavaScript程序是由Unicode字符集编写的，每个字符和汉字都是采用2个字节进行编码。<br/>Unicode码与ASCII码的区别：<br/>ASCII码由大小写英文字母、数字、英文符号等组成，采用1个字节中的低7位进行编码，是Unicode编码的子集；<br/>Unicode是一种国际编码，可以表达几乎任何书写语言，它是采用16位编码的字符集");
